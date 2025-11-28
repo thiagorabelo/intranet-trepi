3e36e0bcc21257328839f80d4e2dd63040e7c0ce
Thiago Rabelo <thiagorabelo1@gmail.com>
2025-11-28 12:17:26 -0300

    fix: Code format

--- backend/tests/setup/test_portal_settings.py
+++ backend/tests/setup/test_portal_settings.py
@@ -1,12 +1,12 @@
 """Portal settings tests"""
 
+from plone import api
+
 import pytest
 
-from plone import api
 
 class TestPortalSettings:
     """Test portal settings"""
-
 
     @pytest.mark.parametrize(
         "key,expected",
@@ -15,10 +15,8 @@ class TestPortalSettings:
             ["plone.email_from_name", "No Reply"],
             ["plone.smtp_host", "10.1.1.2"],
             ["plone.smtp_port", 25],
-        ]
+        ],
     )
     def test_portal_title(self, portal, key: str, expected: str):
         value = api.portal.get_registry_record(key)
-        assert value == expected, (
-            f"Expected {expected}, but got {value} for key {key}"
-        )
+        assert value == expected, f"Expected {expected}, but got {value} for key {key}"
