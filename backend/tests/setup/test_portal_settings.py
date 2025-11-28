10852fbc0a76dc026675bf7d1542b0001d81fa8c
Thiago Rabelo <thiagorabelo1@gmail.com>
2025-11-28 12:14:02 -0300

    feat: Atualia workflow

---
+++ backend/tests/setup/test_portal_settings.py
@@ -0,0 +1,24 @@
+"""Portal settings tests"""
+
+import pytest
+
+from plone import api
+
+class TestPortalSettings:
+    """Test portal settings"""
+
+
+    @pytest.mark.parametrize(
+        "key,expected",
+        [
+            ["plone.site_title", "Intranet do TRE-PI"],
+            ["plone.email_from_name", "No Reply"],
+            ["plone.smtp_host", "10.1.1.2"],
+            ["plone.smtp_port", 25],
+        ]
+    )
+    def test_portal_title(self, portal, key: str, expected: str):
+        value = api.portal.get_registry_record(key)
+        assert value == expected, (
+            f"Expected {expected}, but got {value} for key {key}"
+        )
