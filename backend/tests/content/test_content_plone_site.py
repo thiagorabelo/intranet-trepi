68279c5c8499351a795695528a3e0c7f27bd7d3a
Thiago Rabelo <thiagorabelo1@gmail.com>
2025-12-01 09:41:57 -0300

    Exercício 19 e 20

---
+++ backend/tests/content/test_content_plone_site.py
@@ -0,0 +1,29 @@
+from AccessControl.users import nobody
+from plone import api
+
+import pytest
+
+
+class TestPloneSite:
+    def test_workflow_state(self, portal):
+        """Verifica se o workflow está correto"""
+        expected = "internal"
+        value = api.content.get_state(portal)
+        assert value == expected, f"Expected workflow is {expected}, got {value}"
+
+    @pytest.mark.parametrize(
+        "permission,expected",
+        [
+            ["Access contents information", False],
+            ["Modify portal content", False],
+            ["View", False],
+        ]
+    )
+    def test_anonymous_permissions(self, portal, permission: str, expected: str):
+        with api.env.adopt_user(user=nobody):
+            user = api.user.get_current()
+            print("USUARIO >>> ", user)
+            has_permission = api.user.has_permission(permission, user=user, obj=portal)
+            assert has_permission is expected, (
+                f"Error: Permission {permission} to anounymous user: {has_permission}"
+            )
