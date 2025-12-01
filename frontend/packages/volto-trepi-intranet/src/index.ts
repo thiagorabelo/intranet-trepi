7bc4ba47accb53dbfdfdba2d566576f1dd382457
Thiago Rabelo <thiagorabelo1@gmail.com>
2025-12-01 12:25:38 -0300

    Exercicio 25

--- frontend/packages/volto-trepi-intranet/src/index.ts
+++ frontend/packages/volto-trepi-intranet/src/index.ts
@@ -1,6 +1,6 @@
 import type { ConfigType } from '@plone/registry';
 import installSettings from './config/settings';
-import installViews from './config/settings';
+import installViews from './config/views';
 
 function applyConfig(config: ConfigType) {
   installSettings(config);
