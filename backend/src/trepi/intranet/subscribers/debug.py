0c38f8f05bbd079fe89cdb362d8e77d8d3d3d974
Thiago Rabelo <thiagorabelo1@gmail.com>
2025-12-04 12:03:24 -0300

    Exercício 43 (Bônus)

---
+++ backend/src/trepi/intranet/subscribers/debug.py
@@ -0,0 +1,23 @@
+
+from trepi.intranet import logger
+
+import os
+
+
+def log_event(event: object):
+    """Escreve no log todos os eventos disparados pelo processo do backend.
+
+    Apenas funciona se a variável de ambiente DEBUG estiver definida.
+    exemplo: DEBUG=1 make backend-start
+    """
+    if os.environ.get("DEBUG"):
+        # Caminho do módulo que disparou o evento
+        module_name = event.__class__.__module__
+        # Classe que disparou o evento
+        class_name = event.__class__.__name__
+        # Caminho completo da classe que disparou o evento
+        dotted_name = f"{module_name}.{class_name}"
+        # Escreve no log o evento disparado
+        # Utilizamos `info` para garantir que sempre será exibido,
+        # sem necessidade de alterar o nível do log da aplicação.
+        logger.info(f"- Evento disparado: {dotted_name}")
