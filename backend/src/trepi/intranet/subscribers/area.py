b084251bf60cd9ca5244e27a0753f00b1d227659
Thiago Rabelo <thiagorabelo1@gmail.com>
2025-12-04 10:46:23 -0300

    Exercício 41 - Cria grupo de editores após criação de um objeto do tipo Área

--- backend/src/trepi/intranet/subscribers/area.py
+++ backend/src/trepi/intranet/subscribers/area.py
@@ -1,6 +1,24 @@
+from plone import api
+from Products.PlonePAS.tools.groupdata import GroupData
 from trepi.intranet import logger
 from trepi.intranet.content.area import Area
 from zope.lifecycleevent import ObjectAddedEvent
+
+
+def _cria_grupo_usuarios(obj:Area) -> None:
+    """Cria grupo de usuários para a nova área"""
+    uid = api.content.get_uuid(obj)
+    g_id = f"{uid}-editores"
+    titulo = f"Área {obj.title}: Editores"
+    payload = {
+        "groupname": g_id,
+        "title": titulo,
+        "description": f"Grupo de editores da área {obj.title}",
+    }
+    grupo: GroupData = api.group.create(**payload)
+    logger.info("Criado o grupo '%s' para a área '%s'", titulo, obj.title)
+    api.group.grant_roles(group=grupo, roles=["Editor"], obj=obj)
+    logger.info("Grupo %s recebeu papel de editor em %s", titulo, obj.absolute_url())
 
 
 def _update_excluded_from_nav(obj: Area):
@@ -13,3 +31,4 @@ def _update_excluded_from_nav(obj: Area):
 def added(obj: Area, event: ObjectAddedEvent):
     """Post creation handler for Area."""
     _update_excluded_from_nav(obj)
+    _cria_grupo_usuarios(obj)
