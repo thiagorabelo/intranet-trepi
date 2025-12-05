578202f145d6a2202d9b5e38b555b723cd8beb6a
Thiago Rabelo <thiagorabelo1@gmail.com>
2025-12-05 12:52:50 -0300

    Exercício 49 - Buscar dados no endpoint criado
    Alterar o bloco Clima, para buscar dados do endpoint criado no exercício 46

--- frontend/packages/volto-trepi-intranet/src/index.ts
+++ frontend/packages/volto-trepi-intranet/src/index.ts
@@ -2,11 +2,13 @@ import type { ConfigType } from '@plone/registry';
 import installSettings from './config/settings';
 import installViews from './config/views';
 import installBlocks from './config/blocks';
+import installReducers from 'volto-trepi-intranet/config/reducers';
 
 function applyConfig(config: ConfigType) {
   installSettings(config);
   installViews(config);
   installBlocks(config);
+  installReducers(config);
 
   return config;
 }
